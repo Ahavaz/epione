afca1bc96f50f7bfe1b4e60b3d38dbb9d3350117
Lorensov <lucianlorens@gmail.com>
2018-06-30 05:46:14 -0300

    removed problem

--- src/components/Maps/map.js
+++ src/components/Maps/map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { GeoJSON as LeafletGeoJSON, type LatLng, type Layer } from 'leaflet'
+//import { GeoJSON as LeafletGeoJSON, type LatLng, type Layer } from 'leaflet'
 
 class Map extends Component{
 
