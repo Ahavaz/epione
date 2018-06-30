e37c87ef492a81dea60cda6a263d139e670428cc
Ahavaz <lucas.cesar.8@hotmail.com>
2018-06-30 01:35:10 -0300

    Added chart component responsiveness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Chart from './components/chart/Chart'
 
@@ -7,10 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hacksaúde - Epione</h1>
-        </header>
         <Chart>
         </Chart>
       </div>
--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,45 +1,67 @@
 import React, { Component } from 'react'
 import './Chart.css'
 import '../../../node_modules/react-vis/dist/style.css'
-import { XYPlot, LineSeries, makeWidthFlexible, HorizontalGridLines, VerticalGridLines, XAxis, YAxis } from 'react-vis'
+import { XYPlot, LineSeries, makeVisFlexible, HorizontalGridLines, VerticalGridLines, XAxis, YAxis } from 'react-vis'
+import csv from 'csvtojson'
 
 class Chart extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    const csvFilePath = '/home/lucas/Downloads/ubs.csv'
+    
+    csv().fromFile(csvFilePath)
+      .then((jsonObj => jsonObj.filter(obj => obj.cod_munic === 530010)))
+      .then(jsonPoha => console.log(jsonPoha))
+      .then(jsonFiltered => this.setState({
+        data: jsonFiltered
+    }))
+  }
+
   render() {
-    const data = [
-      { x: 0, y: Math.random() * 30 },
-      { x: 1, y: Math.random() * 30 },
-      { x: 2, y: Math.random() * 30 },
-      { x: 3, y: Math.random() * 30 },
-      { x: 4, y: Math.random() * 30 },
-      { x: 5, y: Math.random() * 30 },
-      { x: 6, y: Math.random() * 30 },
-      { x: 7, y: Math.random() * 30 },
-      { x: 8, y: Math.random() * 30 },
-      { x: 9, y: Math.random() * 30 },
-      { x: 10, y: Math.random() * 30 },
-      { x: 11, y: Math.random() * 30 },
-      { x: 12, y: Math.random() * 30 },
-      { x: 13, y: Math.random() * 30 },
-      { x: 14, y: Math.random() * 30 },
-      { x: 15, y: Math.random() * 30 },
-      { x: 16, y: Math.random() * 30 },
-      { x: 17, y: Math.random() * 30 },
-      { x: 18, y: Math.random() * 30 },
-      { x: 19, y: Math.random() * 30 }
-    ]
+ 
+    // console.log(data)
+    // const data = [
+    //   { x: 0, y: Math.random() * 100 },
+    //   { x: 1, y: Math.random() * 100 },
+    //   { x: 2, y: Math.random() * 100 },
+    //   { x: 3, y: Math.random() * 100 },
+    //   { x: 4, y: Math.random() * 100 },
+    //   { x: 5, y: Math.random() * 100 },
+    //   { x: 6, y: Math.random() * 100 },
+    //   { x: 7, y: Math.random() * 100 },
+    //   { x: 8, y: Math.random() * 100 },
+    //   { x: 9, y: Math.random() * 100 },
+    //   { x: 10, y: Math.random() * 100 },
+    //   { x: 11, y: Math.random() * 100 },
+    //   { x: 12, y: Math.random() * 100 },
+    //   { x: 13, y: Math.random() * 100 },
+    //   { x: 14, y: Math.random() * 100 },
+    //   { x: 15, y: Math.random() * 100 },
+    //   { x: 16, y: Math.random() * 100 },
+    //   { x: 17, y: Math.random() * 100 },
+    //   { x: 18, y: Math.random() * 100 },
+    //   { x: 19, y: Math.random() * 100 }
+    // ]
     
-    const FlexibleXYPlot = makeWidthFlexible(XYPlot)
+    // data.forEach((val, index, array) => {
+    //   array.push({x: index, y: Math.random() * 100})
+    //   console.log(array.x, array.y)
+    // })
     
+    const FlexibleVis = makeVisFlexible(XYPlot)
+
     return (
       <div className="Chart">
-        <FlexibleXYPlot height={450}>
+        <FlexibleVis>
           <VerticalGridLines/>
           <HorizontalGridLines/>
           <XAxis/>
           <YAxis/>
-          <LineSeries data={data}/>
-        </FlexibleXYPlot>
-        {/* <p>{window.height} {window.width}</p> */}
+          <LineSeries data={this.state.data}/>
+        </FlexibleVis>
       </div>
     )
   }
